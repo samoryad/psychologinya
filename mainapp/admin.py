bf3dd422a2450a2064703e9f1ced8384e74484d3
Andrey Samoryadov <samoryad@mail.ru>
2023-11-26 12:36:09 +0300

    изменены модели специалистов (добавлены навыки и образование), изменены шаблоны и вьюшки

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Specialist
+from .models import Specialist, Skill, Education
 
 admin.site.register(Specialist)
+admin.site.register(Skill)
+admin.site.register(Education)
--- mainapp/views.py
+++ mainapp/views.py
@@ -15,20 +15,28 @@ def main(request):
 
 def specialists(request):
     specialist01 = Specialist.objects.get(pk=1)
+    specialist_education01 = specialist01.spec_education.all()
+    specialist_skills01 = specialist01.skills.all()
     content = {
         'title': 'специалисты',
         'links_menu': get_links_menu(),
         'specialist01': specialist01,
+        'specialist_education': specialist_education01,
+        'specialist_skills': specialist_skills01,
     }
     return render(request, 'mainapp/specialists.html', content)
 
 
 def specialist(request, pk=None):
     specialist = Specialist.objects.get(pk=pk)
+    specialist_education = specialist.spec_education.all()
+    specialist_skills = specialist.skills.all()
     content = {
-        'title': 'специалист',
+        'title': f'специалист {specialist.short_name}',
         'links_menu': get_links_menu(),
         'specialist': specialist,
+        'specialist_education': specialist_education,
+        'specialist_skills': specialist_skills,
     }
     return render(request, 'mainapp/specialist.html', content)
 
